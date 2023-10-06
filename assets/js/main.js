129d8199df6b5b66b87df83139362c1201cbef44
Biruk Mesfin <102286732+cypghost@users.noreply.github.com>
2023-10-06 23:32:21 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/**
-* Template Name: ComingSoon
-* Updated: Sep 28 2023 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/comingsoon-free-html-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
